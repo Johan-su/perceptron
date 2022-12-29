a68ca2c2b35af3fa456d38ab6ca9837083571603
Johan-su <jsbollll@gmail.com>
2022-12-30 00:16:26 +0100

    change name

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ set CLANG=clang++
 set FLAGS=-O0 -D _DEBUG -g -gcodeview -Wall -Wpedantic -Wextra -Wconversion -Wshadow -Wno-missing-braces -Wno-c++20-designator -Wno-c++17-extensions -Wno-variadic-macros -Wno-gnu-zero-variadic-macro-arguments
 
 
-%CLANG% ./src/main.cpp %FLAGS% -o chat.exe
+%CLANG% ./src/main.cpp %FLAGS% -o perceptron.exe
 
