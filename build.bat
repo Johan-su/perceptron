5b73f3e623db5123fb2e4907082c7697d6d31ee9
Johan-su <jsbollll@gmail.com>
2022-12-30 00:15:52 +0100

    remove unused libs

--- build.bat
+++ build.bat
@@ -2,9 +2,6 @@
 set CLANG=clang++
 set FLAGS=-O0 -D _DEBUG -g -gcodeview -Wall -Wpedantic -Wextra -Wconversion -Wshadow -Wno-missing-braces -Wno-c++20-designator -Wno-c++17-extensions -Wno-variadic-macros -Wno-gnu-zero-variadic-macro-arguments
 
-set WINSOCKLIB=ws2_32
 
-
-
-%CLANG% ./src/main.cpp -l %WINSOCKLIB% %FLAGS% -o chat.exe
+%CLANG% ./src/main.cpp %FLAGS% -o chat.exe
 
